6253fa93b48604a86b1adc29a3fb1b03b6b6f0bf
Erard <keeliker@gmail.com>
2015-10-14 22:24:14 +0800

    haiyaku

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151008131034) do
+ActiveRecord::Schema.define(version: 20151014122012) do
 
   create_table "ideas", force: :cascade do |t|
     t.string   "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20151008131034) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
